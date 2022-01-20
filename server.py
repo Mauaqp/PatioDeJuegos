44cb19afaaeca27706a8fac65a07ef4bb0999ca8
Mauaqp <peraltilla93@hotmail.com>
2022-01-20 18:25:32 -0500

    Avance

--- server.py
+++ server.py
@@ -2,11 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#Rutas
+#Ruta
 @app.route( '/', methods=['GET'] )
 def paginaInicial():
     return render_template("index.html")
 
+#Ruta - Renderiza 3 cajas azules
+@app.route( '/play/', methods=['GET'] )
+def play():
+    return render_template("index.html")
+
+#Ruta - Renderiza 3 cajas azules
+@app.route( '/play/<int:num>/<string:color>', methods=['GET'] )
+def playNum(num,color):
+    print(color)
+    return impresion * num
 
 #App.run
 if __name__ == "__main__":
