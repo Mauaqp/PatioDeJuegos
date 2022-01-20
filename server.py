70d48a71b957b808e5573d39b6e360b3c4214cea
Mauaqp <peraltilla93@hotmail.com>
2022-01-20 18:50:34 -0500

    Entrega Final

--- server.py
+++ server.py
@@ -7,16 +7,20 @@ app = Flask(__name__)
 def paginaInicial():
     return render_template("index.html")
 
-#Ruta - Renderiza 3 cajas azules
+#Ruta - Nivel 1 
 @app.route( '/play/', methods=['GET'] )
 def play():
-    return render_template("index.html")
+    return render_template("index.html", num = 3, color = "blue")
 
-#Ruta - Renderiza 3 cajas azules
+#Ruta - Renderiza cajas según ordenes numero y color
+@app.route( '/play/<int:num>', methods=['GET'] )
+def playNum(num):
+    return render_template("index.html", num = num, color = "blue")
+
+#Ruta - Renderiza cajas según ordenes numero y color
 @app.route( '/play/<int:num>/<string:color>', methods=['GET'] )
-def playNum(num,color):
-    print(color)
-    return impresion * num
+def playNumCol(num,color):
+    return render_template("index.html", num = num, color = color)
 
 #App.run
 if __name__ == "__main__":
